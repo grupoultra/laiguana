978f93bbc439197f9a575ae6a320e82a1e4c2d1c
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2016-06-30 14:57:50 -0400

    Activando OwlCarrousel

--- activos/helper/js/main.js
+++ activos/helper/js/main.js
@@ -1,7 +1,18 @@
 var i;
 var divs = document.getElementsByClassName('titulo-wrap');
 for(i=0;i<divs.length;i++) {
-    if(divs[i].innerHTML.length > 75){
-        divs[i].innerHTML = divs[i].innerHTML.substring(0,75) + '...';
+    if(divs[i].innerHTML.length > 65){
+        divs[i].innerHTML = divs[i].innerHTML.substring(0,65) + '...';
     }
 }
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        autoplay: true,
+        autoplayTimeout:5000,
+        autoplayHoverPause: true,
+        nav: true,
+        navText: ['<','>'],
+        dotsEach: true
+    });
+});
