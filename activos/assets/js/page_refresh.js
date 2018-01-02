2cbb1ef0af262265375777871440dcb7175a6c09
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-01-02 13:12:21 -0400

    Refactors el_video_isActive to youtube_isActive

--- activos/assets/js/page_refresh.js
+++ activos/assets/js/page_refresh.js
@@ -1,7 +1,7 @@
 var pageRefreshMinutes = 0.5;
 
 window.setTimeout(function() {
-    if (el_video_isActive || litv_isActive) {
+    if (youtube_isActive) {
         console.log(
             "El video o LITV se está reproduciendo, no se recarga el site"
         );
