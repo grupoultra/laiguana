22481541d1327b5781b956695339de16ffc2cffe
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-01-02 13:12:08 -0400

    Refactors el_video_isActive to youtube_isActive

--- activos/assets/js/el_video.js
+++ activos/assets/js/el_video.js
@@ -2,15 +2,12 @@
 // More on: https://developers.google.com/youtube/iframe_api_reference
 
 var el_video_player;
-var el_video_isActive = false;
-var litv_isActive = false;
+var youtube_isActive = false;
 
 //this function is called by the API
 function onYouTubeIframeAPIReady() {
     //creates the player object
     el_video_player = new YT.Player("el_video_iframe");
-
-    console.log("Video API is loaded");
 
     //subscribe to events
     el_video_player.addEventListener(
@@ -24,27 +21,27 @@ function onElVideoPlayerStateChange(event) {
     switch (event.data) {
         case YT.PlayerState.UNSTARTED:
             console.log("el video unstarted");
-            el_video_isActive = false;
+            youtube_isActive = false;
             break;
         case YT.PlayerState.ENDED:
             console.log("el video ended");
-            el_video_isActive = false;
+            youtube_isActive = false;
             break;
         case YT.PlayerState.PLAYING:
             console.log("el video playing");
-            el_video_isActive = true;
+            youtube_isActive = true;
             break;
         case YT.PlayerState.PAUSED:
-            el_video_isActive = false;
+            youtube_isActive = false;
             console.log("el video paused");
             break;
         case YT.PlayerState.BUFFERING:
-            el_video_isActive = true;
+            youtube_isActive = true;
 
             console.log("el video buffering");
             break;
         case YT.PlayerState.CUED:
-            el_video_isActive = false;
+            youtube_isActive = false;
 
             console.log("el video video cued");
             break;
