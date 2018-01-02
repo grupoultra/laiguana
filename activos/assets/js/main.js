7f7d6c3b0b8f9c9657835247126d383e2460ac9b
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-01-02 10:30:18 -0400

    Reformatting

--- activos/assets/js/main.js
+++ activos/assets/js/main.js
@@ -6,15 +6,15 @@ function wordCount(str) {
     return getWords(str).length;
 }
 
-function getWords(str){
+function getWords(str) {
     return str.split(" ");
 }
 
-function sliceWords(str){
+function sliceWords(str) {
     return str.slice(0, wordsLimit);
 }
 
-function joinEllipsis(str){
+function joinEllipsis(str) {
     return str.join(" ") + "...";
 }
 
@@ -24,114 +24,131 @@ function share(platform, link, text){
         case 'FB':
             url = 'https://www.facebook.com/sharer/sharer.php?u=http://laiguana.tv' + link;
             break;
-        case 'TW':
-            url = 'https://twitter.com/home?status='+ text +' http://laiguana.tv' + link + ' @la_iguanatv';
+        case "TW":
+            url =
+                "https://twitter.com/home?status=" +
+                text +
+                " http://laiguana.tv" +
+                link +
+                " @la_iguanatv";
             break;
-        case 'GP':
-            url = 'https://plus.google.com/share?url=http://laiguana.tv' + link;
+        case "GP":
+            url = "https://plus.google.com/share?url=http://laiguana.tv" + link;
             break;
     }
-    newwindow=window.open(url,'name','height=300,width=600');
-    if (window.focus) {newwindow.focus()}
+    newwindow = window.open(url, "name", "height=300,width=600");
+    if (window.focus) {
+        newwindow.focus();
+    }
     return false;
-};
+}
 
 function setCookie(cname, cvalue, exminutes, session) {
-    console.log('Setting cookie ', cname, 'to', cvalue);
+    console.log("Setting cookie ", cname, "to", cvalue);
     var d = new Date();
-    d.setTime(d.getTime() + (exminutes*60*1000));
-    var expires = session ? "" : ";" + "expires="+ d.toUTCString() + ";path=/";
-    document.cookie = cname + "=" + cvalue + expires ;
+    d.setTime(d.getTime() + exminutes * 60 * 1000);
+    var expires = session ? "" : ";" + "expires=" + d.toUTCString() + ";path=/";
+    document.cookie = cname + "=" + cvalue + expires;
 }
 function cookieValid(cname) {
-    return getCookie(cname) != ""
+    return getCookie(cname) != "";
 }
 
 function getCookie(cname) {
     var name = cname + "=";
-    var ca = document.cookie.split(';');
+    var ca = document.cookie.split(";");
 
-    for(var i = 0; i <ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') {
+        while (c.charAt(0) == " ") {
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
+            return c.substring(name.length, c.length);
         }
     }
     return "";
 }
 
-function share(platform, link, text){
-    var url = '';
-    switch (platform){
-        case 'FB':
-            url = 'https://www.facebook.com/sharer/sharer.php?u=http://laiguana.tv' + link;
+function share(platform, link, text) {
+    var url = "";
+    switch (platform) {
+        case "FB":
+            url =
+                "https://www.facebook.com/sharer/sharer.php?u=http://laiguana.tv" +
+                link;
             break;
-        case 'TW':
-            url = 'https://twitter.com/home?status='+ text +' http://laiguana.tv' + link + ' @la_iguanatv';
+        case "TW":
+            url =
+                "https://twitter.com/home?status=" +
+                text +
+                " http://laiguana.tv" +
+                link +
+                " @la_iguanatv";
             break;
-        case 'GP':
-            url = 'https://plus.google.com/share?url=http://laiguana.tv' + link;
+        case "GP":
+            url = "https://plus.google.com/share?url=http://laiguana.tv" + link;
             break;
     }
-    newwindow=window.open(url,'name','height=300,width=600');
-    if (window.focus) {newwindow.focus()}
+    newwindow = window.open(url, "name", "height=300,width=600");
+    if (window.focus) {
+        newwindow.focus();
+    }
     return false;
-};
-    
+}
+
 function setCookie(cname, cvalue, exminutes, session) {
-    console.log('Setting cookie ', cname, 'to', cvalue);
+    console.log("Setting cookie ", cname, "to", cvalue);
     var d = new Date();
-    d.setTime(d.getTime() + (exminutes*60*1000));
-    var expires = session ? "" : ";" + "expires="+ d.toUTCString() + ";path=/";
-    document.cookie = cname + "=" + cvalue + expires ;
+    d.setTime(d.getTime() + exminutes * 60 * 1000);
+    var expires = session ? "" : ";" + "expires=" + d.toUTCString() + ";path=/";
+    document.cookie = cname + "=" + cvalue + expires;
 }
 function cookieValid(cname) {
-    return getCookie(cname) != ""
+    return getCookie(cname) != "";
 }
 
 function getCookie(cname) {
     var name = cname + "=";
-    var ca = document.cookie.split(';');
+    var ca = document.cookie.split(";");
 
-    for(var i = 0; i <ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') {
+        while (c.charAt(0) == " ") {
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
+            return c.substring(name.length, c.length);
         }
     }
     return "";
 }
 
-
-for(i=0;i<divs.length;i++) {
-    if(wordCount(divs[i].innerHTML) > wordsLimit){
-        divs[i].innerHTML = joinEllipsis(sliceWords(getWords(divs[i].innerHTML)));
+for (i = 0; i < divs.length; i++) {
+    if (wordCount(divs[i].innerHTML) > wordsLimit) {
+        divs[i].innerHTML = joinEllipsis(
+            sliceWords(getWords(divs[i].innerHTML))
+        );
     }
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
     $(".owl-carousel").owlCarousel({
         autoplay: false,
-        autoplayTimeout:5000,
+        autoplayTimeout: 5000,    Refactors el_video_isActive to youtube_isActive
         autoplayHoverPause: true,
         nav: true,
-        navText: ['<','>'],
+        navText: ["<", ">"],
         dotsEach: true,
         slideBy: 3,
         loop: true,
         lazyLoad: true,
         dots: false,
-        responsive : {
-            0 : {
+        responsive: {
+            0: {
                 items: 2
             },
-            1090 : {
+            1090: {
                 items: 3
             }
         }
@@ -140,7 +157,7 @@ $(document).ready(function(){
     $(".owl-carousel-destacados").owlCarousel({
         autoplay: true,
         items: 1,
-        autoplayTimeout:5000,
+        autoplayTimeout: 5000,
         autoplayHoverPause: true,
         dotsEach: true,
         nav: true,
@@ -155,7 +172,7 @@ $(document).ready(function(){
     $(".owl-carousel-television").owlCarousel({
         autoplay: true,
         items: 1,
-        autoplayTimeout:5000,
+        autoplayTimeout: 5000,
         autoplayHoverPause: true,
         dotsEach: true,
         nav: true,
@@ -199,13 +216,14 @@ $(document).ready(function(){
 //     }
 // });
 
-$('.marquee').marquee({
+$(".marquee").marquee({
     pauseOnHover: true,
-    duration: 10000,  
+    duration: 10000,
     startVisible: false
 });
-const twitterButton = document.querySelector('.twitter-shar-button');
+const twitterButton = document.querySelector(".twitter-shar-button");
 
-if(twitterButton){
-    twitterButton.innerHTML ='<img src="/activos/assets/img/tw-share-tag.png" alt="La Iguana Twitter" />';
+if (twitterButton) {
+    twitterButton.innerHTML =
+        '<img src="/activos/assets/img/tw-share-tag.png" alt="La Iguana Twitter" />';
 }
