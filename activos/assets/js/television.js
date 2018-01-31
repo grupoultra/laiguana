b78e061e24555bede4348ba1439f3ca8a4d47f60
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-01-31 11:47:38 -0400

    Show LITV on mobile and adapts its design

--- activos/assets/js/television.js
+++ activos/assets/js/television.js
@@ -156,38 +156,36 @@ function loadPlaylistItem(item, index) {
 }
 
 $(document).ready(function() {
-    if ($(window).width() > 1090) {
-        Promise.all(
-            playlists.map(function(playlist) {
-                return getVids(playlist.id);
-            })
-        )
-            .then(function(results) {
-                allPlaylists = results;
-                loadPlayer(0);
+    Promise.all(
+        playlists.map(function(playlist) {
+            return getVids(playlist.id);
+        })
+    )
+        .then(function(results) {
+            allPlaylists = results;
+            loadPlayer(0);
 
-                loadPlaylists();
-            })
-            .then(function() {
-                console.log("=============================");
-                $(".oc-television").owlCarousel({
-                    autoplay: true,
-                    navText: ["<", ">"],
-                    items: 3,
-                    autoplayTimeout: 2000,
-                    autoplayHoverPause: true,
-                    pauseOnHover: true,
-                    dotsEach: true,
-                    nav: true,
-                    loop: true,
-                    dots: false
-                });
-            })
-            .then(function() {
-                console.log("LITV fully charged");
-            })
-            .catch(function(error) {
-                console.log("********", error);
+            loadPlaylists();
+        })
+        .then(function() {
+            console.log("=============================");
+            $(".oc-television").owlCarousel({
+                autoplay: true,
+                navText: ["<", ">"],
+                items: 3,
+                autoplayTimeout: 2000,
+                autoplayHoverPause: true,
+                pauseOnHover: true,
+                dotsEach: true,
+                nav: true,
+                loop: true,
+                dots: false
             });
-    }
+        })
+        .then(function() {
+            console.log("LITV fully charged");
+        })
+        .catch(function(error) {
+            console.log("********", error);
+        });
 });
